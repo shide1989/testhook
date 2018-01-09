b30c108091a4a066375f1b5cb5202cc30bd4f40b
Sebastien Hideux <hideux.sebastien@gmail.com>
2018-01-09 19:21:40 +0100

    test cron 5

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-    res.send('Hello World! v2')
+    res.send('Hello World! v5')
 })
 
 app.listen(3000, function () {
