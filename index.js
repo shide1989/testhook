9568e9e4033528f12fcbe7d2c4793f57feb33d90
Sebastien Hideux <hideux.sebastien@gmail.com>
2018-01-18 15:17:16 +0100

    updated to version 9

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-    res.send('Hello World! v8')
+    res.send('Hello World! v9')
 })
 
 app.listen(3000, function () {
