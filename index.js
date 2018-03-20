3dd8e6e35d6720b8549628c7377d64fa3d6f6330
Sebastien Hideux <hideux.sebastien@gmail.com>
2018-03-20 14:44:50 +0100

    tests v9-snapshoot

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-    res.send('Hello World! v9')
+    res.send('Hello World! v9-snApshOoot')
 })
 
 app.listen(3000, function () {
